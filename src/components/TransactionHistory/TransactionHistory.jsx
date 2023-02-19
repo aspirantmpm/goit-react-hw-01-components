b35ab79fece761d7d51d17c8a292fe39cf26aa7d
Vadym <aspirantmpm@gmail.com>
2023-02-19 13:29:03 +0200

    Transaction styles

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,25 +1,33 @@
 // import { TransactionItem } from './TransactionItem';
+import { Table } from './TransactionHistory.styled';
+import { Thead } from './TransactionHistory.styled';
+import { Tr } from './TransactionHistory.styled';
+import { Th } from './TransactionHistory.styled';
+import { Td } from './TransactionHistory.styled';
+import { Section } from './TransactionHistory.styled';
 
 export const TransactionHistory = ({ items }) => {
   return (
-    <table className="transaction-history">
-      <thead>
-        <tr>
-          <th>Type</th>
-          <th>Amount</th>
-          <th>Currency</th>
-        </tr>
-      </thead>
+    <Section>
+      <Table>
+        <Thead>
+          <Tr>
+            <Th>Type</Th>
+            <Th>Amount</Th>
+            <Th>Currency</Th>
+          </Tr>
+        </Thead>
 
-      <tbody>
-        {items.map(items => (
-          <tr key={items.id}>
-            <td>{items.type}</td>
-            <td>{items.amount}</td>
-            <td>{items.currency}</td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
+        <tbody>
+          {items.map(items => (
+            <Tr key={items.id}>
+              <Td>{items.type}</Td>
+              <Td>{items.amount}</Td>
+              <Td>{items.currency}</Td>
+            </Tr>
+          ))}
+        </tbody>
+      </Table>
+    </Section>
   );
 };
---
+++ src/components/TransactionHistory/TransactionHistory.styled.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const Section = styled.section`
+  margin: 20px;
+`;
+export const Table = styled.table`
+  border: 3px solid black;
+  text-align: center;
+`;
+export const Thead = styled.thead`
+  background-color: #625df580;
+`;
+export const Tr = styled.tr`
+  border: 3px solid black;
+`;
+export const Th = styled.th`
+  border: 3px solid black;
+`;
+export const Td = styled.td`
+  border: 1px solid black;
+`;
